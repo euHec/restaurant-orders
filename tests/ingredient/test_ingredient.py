d7ceabf14f34e6602627b2910ce07f56cba75c98
euHec <hectorsouzass@gmail.com>
2024-01-14 11:42:11 -0300

    Corrige implementação dos tests

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,72 +1,27 @@
 import unittest
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
-
-ANIMAL_DERIVED = "ANIMAL_DERIVED"
-ANIMAL_MEAT = "ANIMAL_MEAT"
-SEAFOOD = "SEAFOOD"
-LACTOSE = "LACTOSE"
-GLUTEN = "GLUTEN"
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
-
-
-def test_same_ingredient_has_same_hash():
-    ingredient1 = Ingredient("queijo mussarela")
-    ingredient2 = Ingredient("queijo mussarela")
-    assert hash(ingredient1) == hash(ingredient2)
-
-
-def test_different_ingredient_has_different_hash():
     ingredient1 = Ingredient("queijo mussarela")
     ingredient2 = Ingredient("bacon")
+    ingredient3 = Ingredient("bacon")
+
+    assert ingredient1.name == "queijo mussarela"
+    assert ingredient1.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert hash(ingredient1) == hash(ingredient1)
     assert hash(ingredient1) != hash(ingredient2)
-
-
-def test_equal_ingredients_are_equal():
-    ingredient1 = Ingredient("queijo mussarela")
-    ingredient2 = Ingredient("queijo mussarela")
-    assert ingredient1 == ingredient2
-
-
-def test_different_ingredients_are_not_equal():
-    ingredient1 = Ingredient("queijo mussarela")
-    ingredient2 = Ingredient("bacon")
+    assert hash(ingredient2) == hash(ingredient3)
     assert ingredient1 != ingredient2
-
-
-def test_repr_returns_correct_value():
-    ingredient = Ingredient("queijo mussarela")
-    assert repr(ingredient) == "Ingredient('queijo mussarela')"
-
-
-def test_name_attribute_correct():
-    ingredient = Ingredient("queijo mussarela")
-    assert ingredient.name == "queijo mussarela"
-
-
-def test_restrictions_attribute_correct():
-    ingredient = Ingredient("queijo mussarela")
-    expected_restrictions = {LACTOSE, ANIMAL_DERIVED}
-    assert ingredient.restrictions == expected_restrictions
-
-
-def test_incorrect_name_attribute_fails():
-    ingredient = Ingredient("queijo mussarela")
-    expected_name = "queijo cheddar"
-    assert (
-        ingredient.name == expected_name
-    ), f"Expected: {expected_name}, Actual: {ingredient.name}"
-
-
-def test_incorrect_restrictions_attribute_fails():
-    ingredient = Ingredient("queijo mussarela")
-    expected_restrictions = {LACTOSE, GLUTEN}
-    assert (
-        ingredient.restrictions == expected_restrictions
-    ), f"Expected: {expected_restrictions}, Actual: {ingredient.restrictions}"
+    assert ingredient1 == ingredient1
+    assert repr(ingredient1) == "Ingredient('queijo mussarela')"
 
 
 if __name__ == "__main__":
